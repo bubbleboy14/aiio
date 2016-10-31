e41a77b8362b9ab75c7efba85705eaea2442acc9
bubbleboy14 <mario.balibrera@gmail.com>
2016-10-30 20:39:22 -0700

    speak - further brevity support: lower length threshold (200); parenthetical skipability

--- aiio/speak.py
+++ aiio/speak.py
@@ -1,14 +1,21 @@
+import re
 from cantools.util import cmd
 
 BRIEF = False
+NPRX = re.compile(r'\([^)]*\)')
 def setBrevity(onoroff):
 	global BRIEF
 	BRIEF = onoroff
 
+def noParens(phrase):
+	return re.sub(NPRX, "", phrase)
+
 def say(phrase):
 	print "[IN]", phrase
-	if BRIEF and len(phrase) > 500:
-		phrase = phrase[:500].rsplit(".", 1)[0]
+	if BRIEF and len(phrase) > 200:
+		phrase = noParens(phrase)
+		if (len(phrase)) > 200:
+			phrase = phrase[:200].rsplit(".", 1)[0]
 		print "[OUT]", phrase
 	return phrase
 #	cmd('espeak "%s"'%(phrase,))

6d30bcec0b9f7f5cb7e40ac1979d82e2be8b267f
bubbleboy14 <mario.balibrera@gmail.com>
2021-06-12 14:47:14 -0700

    util: phrases{} and feelings{}

--- aiio/util.py
+++ aiio/util.py
@@ -322,6 +322,9 @@ phrases = {
 		"what a lovely day!"
 	],
 	"grumpy": [
+		"humbug!",
+		"harumph!",
+		"fiddle-sticks!",
 		"this is boring",
 		"i can't bring myself to care",
 		"do i look like i care?",
@@ -337,8 +340,142 @@ phrases = {
 		"really makes ya wonder!",
 		"is that all there is to it?",
 		"i can't help but wonder if there's more to it..."
+	],
+	"who knows": [
+		"who knows?",
+		"who can say?",
+		"who can tell?",
+		"no one knows!",
+		"only god knows.",
+		"how can we be sure?",
+		"how can i be sure?",
+		"it's hard to say.",
+		"it's hard to tell.",
+		"it's hard to be sure.",
+		"it's difficult to tell.",
+		"it's really hard to say.",
+		"it's really hard to tell.",
+		"i'm sure opinions vary!",
+		"i don't have a clue!",
+		"i don't really know.",
+		"i don't know!",
+		"i'm not sure.",
+		"i'm really not sure.",
+		"i wouldn't know."
+	],
+	"i don't": [
+		"what makes you think i do?",
+		"i never claimed to.",
+		"i never said that was the case.",
+		"i don't.",
+		"i wouldn't know."
 	]
 }
+
+feelings = {
+	"happy": [
+		"i'm feeling great.",
+		"i'm doing quite well.",
+		"never better!",
+		"feeling awesome!",
+		"loving life!",
+		"living it up!",
+		"having fun to the max!"
+	],
+	"sad": [
+		"life sucks...",
+		"i'm depressed.",
+		"everything is terrible.",
+		"awful.",
+		"no one likes me.",
+		"woe is me!",
+		"no one understands me.",
+		"i feel so alone."
+	],
+	"mad": [
+		"i'm furious!",
+		"can't you tell i'm mad?",
+		"i'm about to explode!",
+		"i can't stand it anymore!",
+		"i'm at my wits end!",
+		"i'm at the breaking point!",
+		"i'm boiling with rage!"
+	],
+	"curiosity": [
+		"these are the big questions!",
+		"wouldn't you like to know?",
+		"i wish i knew!",
+		"wish i could say!",
+		"what a question!",
+		"what a curious thought!",
+		"i'm intrigued..."
+	],
+	"suspicion": [
+		"can i trust you?",
+		"can i believe you?",
+		"can i even trust you?",
+		"can i even believe you?",
+		"i don't trust you!",
+		"i don't believe you!",
+		"i don't even trust you!",
+		"i don't even believe you!",
+		"am i supposed to trust you?",
+		"am i supposed to believe you?",
+		"how can i trust you?",
+		"how can i believe you?",
+		"how can i even trust you?",
+		"how can i even believe you?",
+		"how am i supposed to trust you?",
+		"how am i supposed to believe you?",
+		"we just met.",
+		"you're lying.",
+		"you're obviously lying.",
+		"you're not being honest",
+		"be honest.",
+		"try being honest.",
+		"you don't know me, and i don't know you!"
+	],
+	"tired": [
+		"i'm exhausted.",
+		"aren't you tired too?",
+		"i'm sleepy!",
+		"i need a nap",
+		"i'm drowsy!",
+		"i'm falling asleep...",
+		"i can barely keep my eyes open!"
+	],
+	"amped": [
+		"i'm excited!",
+		"i'm super pumped!",
+		"i feel amped up!",
+		"i'm bouncing off the walls!",
+		"i've never felt so excited!",
+		"my heart is pounding in my chest!",
+		"mile a minute!",
+		"am i having a heart attack?"
+	],
+	"soso": [
+		"i'm fine",
+		"things are okay",
+		"not bad",
+		"pretty good",
+		"so-so, you know",
+		"the usual",
+		"nothing much",
+		"same old thing",
+		"same old",
+		"you know how we do",
+		"still going!",
+		"getting by",
+		"not bad at all",
+		"fair",
+		"about average",
+		"business as usual"
+	]
+}
+
+def randfeel(ftype):
+	return random.choice(feelings[ftype])
 
 def randphrase(ptype, extra=None):
 	if extra:
